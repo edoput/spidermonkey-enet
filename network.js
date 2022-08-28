516e3ee2038d6e3f3e40498e1a5d54c7c845035f
Edoardo Putti <edoardo.putti@gmail.com>
2022-08-28 16:28:09 +0200

    update network script to use two handlers

--- network.js
+++ network.js
@@ -1,27 +1,25 @@
-// this is the network script that we load where we will register the callbacks
+// this is the network script that we load where we will register the callback
+// for the networking events
+var enet_states = [
+        "disconnected",
+        "ack-connect",
+        "connection-pending",
+        "connection-succeeded",
+        "connected",
+        "disconnect-later",
+        "disconnecting",
+        "ack-disconnect",
+        "zombie",
+];
 
-// there will be a global object called network
-// there will be one two classes, one bound to ENetPeer
-// the other to ENetHost. Both classes are not available
-// to JS and instances of that type are available only
-// as arguments to the callbacks.
+network.server = function (eventName, host, peer) {
+    console.log("received event ", eventName);
+    console.log("peer address ", peer.address);
+    console.log("peer bandwidth ", "out: ", peer.bandwidth.outgoing, " incoming: ", peer.bandwidth.incoming);
+    console.log("peer state ", enet_states[peer.state]);
+};
 
-// we expect to have the following function signatures
-// network.registerEventListener("connect", (host, client) => undefined);
-// network.removeEventListener("connect, (host, client) => undefined);
-// network.dispatch("connect");
 
-// network.registerEventListener("disconnect", (host, client) => undefined);
-// network.removeEventListener("disconnect, (host, client) => undefined);
-// network.dispatch("disconnect");
-
-// network.registerEventListener("message", (host, client) => undefined);
-// network.removeEventListener("message, (host, client) => undefined);
-// network.dispatch("message");
-
-function noop () {};
-
-netserver.addEventListener("connect", (host, peer) => {
-    console.log("new connection!");
-    console.log(host);
-});
+network.client = function (eventName, host, peer) {
+    console.log("received event ", eventName);
+}
